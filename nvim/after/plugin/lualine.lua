7cfb6ff32dc11f0b57165408bb4ccfb7d6ff09db
David Parrott <david.parrott@law.byu.edu>
2023-08-07 14:17:34 -0600

    nvim(lualine): add work config

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,50 +1,54 @@
 require('lualine').setup({
-    options = {
-        theme = 'codedark',
-        globalstatus = true,
+  options = {
+    theme = 'auto',
+    globalstatus = true,
+    always_divide_middle = false,
+    component_separators = { left = '', right = '' }
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = {
+      'branch',
+      --{ 'diagnostics', sources = { 'nvim_lsp' } }
     },
-    sections = {
-        lualine_a = { 'mode' },
-        lualine_b = {
-            'branch',
-            { 'diagnostics', sources = { 'nvim_lsp' } }
-        },
-        lualine_c = {
-            {
-                'filetype',
-                icon = { align = 'right' },
-                icon_only = true,
-                colored = false,
-                separator = '',
-                padding = { left = 1, right = 0 }
-            },
-            {
-                'filename',
-                separator = '',
-                padding = { left = 1, right = 1 }
-            }
-        },
-        lualine_x = {
-            {
-                'fileformat',
-                symbols = {
-                    unix = 'lf',
-                    dos = 'crlf',
-                    mac = 'lf'
-                },
-                color = { fg = '#696969' }
-            },
-        },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
+    lualine_c = {
+      '%=',
+      {
+        'filetype',
+        icon = { align = 'right' },
+        icon_only = true,
+        colored = false,
+        separator = '',
+        padding = { left = 1, right = 1 }
+      },
+      {
+        'filename',
+        separator = '',
+        padding = { left = 1, right = 1 }
+      }
     },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {}
+    lualine_x = {
+      {
+        'fileformat',
+        symbols = {
+          unix = 'lf',
+          dos = 'crlf',
+          mac = 'lf'
+        },
+        color = { fg = '#696969' }
+      },
     },
-    extensions = { 'fzf' }
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  extensions = { 'quickfix' }
 })
+
