3c2adfe9ce8dbbc9bab9630a399c1f6efb6fa325
deparr <dparrott@protonmail.com>
2025-04-20 13:01:13 -0600

    .

--- zsh/include/aliases.zsh
+++ zsh/include/aliases.zsh
@@ -5,51 +5,60 @@ alias s='source ~/.config/zsh/zshrc'
 for c in sv shutdown poweroff reboot ; do
     alias $c="sudo $c"
 done; unset c
-alias sleep="sudo zzz"
-
-# convenience
-#alias \
-#    ls='ls --color=auto' \
-#    ll='ls -al --color=auto' \
-#    la='la -A --color=auto' \
-#    l='ls -CF --color=auto' \
-#    l.='ls -d .* --color=auto'
+# alias sleep="sudo zzz"
 
 alias \
-    ls='exa -s=Name --color=auto' \
-    ll='exa -s=Name -al --color=auto' \
-    la='exa -s=Name -a --color=auto' \
-    l='exa -F -s=Name --color=auto' \
-    l.='exa -d -s=Name --color=auto .*' \
+    ls='eza -s=Name --color=auto' \
+    ll='eza -s=Name -al --color=auto' \
+    la='eza -s=Name -a --color=auto' \
+    l='eza -F -s=Name --color=auto' \
+    l.='eza -d -s=Name --color=auto .*' \
     diff="diff --color=auto" \
     rg="rg --color auto" \
     bc="bc -ql" \
 
-# remaps
 alias \
     vim='nvim' \
     vimdiff='nvim -d' \
+    gvim='nvim --listen /tmp/godot.pipe' \
     lf='lfrun' \
-    cat='bat -n ' \
-    campus='ssh dmparr22@schizo.cs.byu.edu' \
-    campusx='ssh -X dmparr22@schizo.cs.byu.edu' \
-    zh='history|grep '
+    cat='bat' \
+	zh='nvim ~/.local/zsh_history' \
+	cloc='scc --no-cocomo' \
+	scc='scc --no-cocomo'
+    #zhs='rg ~/.local/zsh_history'
 
 # xbps
 alias \
     xi='sudo xbps-install -S' \
     xu='sudo xbps-install -Su' \
     xq='xbps-query' \
+    xs='xbps-query -R' \
     xr='sudo xbps-remove -R'
 
 alias diskspace='du -hS | sort -n -r | more'
 
-# borken keyboard aliases
+alias icat='wezterm imgcat'
+
+# git
 alias \
-    jit='git' \
-    jo='go' \
-    jcc='gcc' \
-    j++='g++' \
-    ytop='htop' \
-    py='python' \
+	gs='git status' \
+	gwip='git add -A; git rm $(git ls-files) 2> /dev/null; git commit -m "[WIP]: $(date)"' \
+	gpull='git pull' \
+	gpush='git push' \
+	gd='git diff' \
+	gpm='git pull --merge' \
+	gundo='git reset HEAD~'
+
+grbi() {
+	git rebase -i "HEAD~$1"
+}
+
+# broken keyboard aliases
+alias \
+    jit='git ' \
+    jo='go ' \
+    jcc='gcc ' \
+    j++='g++ ' \
+    ytop='htop ' \
 
--- zsh/include/functions.zsh
+++ zsh/include/functions.zsh
@@ -4,17 +4,18 @@
 extract () {
      if [ -f $1 ] ; then
          case $1 in
-             *.tar.bz2)   tar xjf $1        ;;
-             *.tar.gz)    tar xzf $1     ;;
-             *.bz2)       bunzip2 $1       ;;
-             *.rar)       rar x $1     ;;
-             *.gz)        gunzip $1     ;;
-             *.tar)       tar xf $1        ;;
-             *.tbz2)      tar xjf $1      ;;
-             *.tgz)       tar xzf $1       ;;
-             *.zip)       unzip $1     ;;
-             *.Z)         uncompress $1  ;;
-             *.7z)        7z x $1    ;;
+             *.tar.bz2)   tar xjf $1 ;;
+             *.tar.gz)    tar xzf $1 ;;
+             *.bz2)       bunzip2 $1 ;;
+             *.rar)       rar x $1   ;;
+             *.gz)        gunzip $1  ;;
+             *.tar)       tar xf $1  ;;
+             *.tbz2)      tar xjf $1 ;;
+             *.tgz)       tar xzf $1 ;;
+             *.zip)       unzip $1   ;;
+             *.Z)         uncompress $1 ;;
+             *.7z)        7z x $1 ;;
+             *.xz)        unxz $1 ;;
              *)           echo "'$1' cannot be extracted via extract()" ;;
          esac
      else
@@ -22,13 +23,39 @@ extract () {
      fi
 }
 
+# don't source node at startup
 function snode() {
-    source $XDG_CONFIG_HOME/zsh/plugin/zsh-nvm/zsh-nvm.zsh
+    source $XDG_CONFIG_DIR/zsh/plugin/zsh-nvm/zsh-nvm.plugin.zsh
 }
 
-# git clone / update
-function git_clone_or_update() {
-    git clone "$1" "$2" 2>/dev/null && print 'Updated' || (cd "$2"; git pull)
+function geometry_bun() {
+    test -f bun.lockb || return 1
+    GEOMETRY_BUN=$(ansi yellow ${GEOMETRY_BUN_SYMBOL="󰋙"})
+    echo -n "$GEOMETRY_BUN v$(bun --version)"
+}
+
+function geometry_go() {
+    test -f go.mod || test -f go.sum || return 1
+    GEOMETRY_GO=$(ansi cyan ${GEOMETRY_GO_SYMBOL="󰋙"})
+    local go_ver="$(go version | grep --color=never -oE '[0-9]+\.[0-9]+\.[0-9]')"
+    echo -n "$GEOMETRY_GO v$go_ver"
+}
+
+function geometry_zig() {
+    test -f build.zig || test -f build.zig.zon || return 1
+    GEOMETRY_ZIG=$(ansi yellow ${GEOMETRY_ZIG_SYMBOL=""})
+    echo -n "$GEOMETRY_ZIG v$(zig version)"
+}
+
+function geometry_rust() {
+    test -f Cargo.toml || return 1
+    GEOMETRY_RUST=$(ansi red ${GEOMETRY_RUST_SYMBOL="󰋘"})
+    local rust_ver="$(rustc --version | grep --color=never -oE '[0-9]+\.[0-9]+\.[0-9]')"
+    echo -n "$GEOMETRY_RUST v$rust_ver"
+}
+
+function geometry_better_jobs() {
+    [[ 0 -ne "$(jobs -s | wc -l)" ]] && ansi magenta '%(j.✦%j.fj)'
 }
 
 function better_ctrl_z() {
@@ -41,6 +68,7 @@ function better_ctrl_z() {
         zle accept-line
     fi
 }
+
 
 zle -N better_ctrl_z
 bindkey '^z' better_ctrl_z
