a34114f21a0627d9127298e15a25a23c3cb40e9d
deparr <dparrott@protonmail.com>
2025-11-11 14:58:41 -0700

    update session script

--- bin/session.ps1
+++ bin/session.ps1
@@ -1,14 +1,9 @@
 $root = $env:DEV_ROOT ?? "v:\dev"
-if ($args[0] -eq "-b" -or $args[0] -eq "--build") {
-    $root = $env:BUILD_ROOT ?? "v:\build"
-    $args = $args[1..($args.Length - 1)]
-}
-
 $proj = $null
 if (-not [string]::IsNullOrEmpty($args[0]) -and (test-path (join-path $root $args[0]))) {
     $proj = join-path $root $args[0]
 } else {
-    $proj = fd --exact-depth 2 --type d . $root | & $env:FILTER_PROG
+    $proj = fd --exact-depth 2 --type d  --color never . $root | & $env:FILTER_PROG
     if ($lastexitcode -ne "0") {
         exit 0
     }
